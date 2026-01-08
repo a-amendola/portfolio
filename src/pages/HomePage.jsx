9d8bcce29575c1cdb354fb2b641e0649b2417594
Ashley Amendola <ama7674@psu.edu>
2026-01-08 20:59:53 +0530

    Hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
+import './homepage.css'
+import ashleyImage from '../assets/ashley.JPG'
+
 export function HomePage() {
     return (
-        <div className="home-page">
-            <h1>Hello World</h1>
-        </div>
+        <section className="home-page flex">
+            <div className="hero flex">
+                <div className="hero-left">
+                    <img src={ashleyImage} alt="Ashley" />
+                </div>
+                <div className="hero-right">
+                    <h1>Your business deserves a better website.</h1>
+                    <h2>I'm Ashley, a website developer focused on modern, clean design.</h2>
+                </div>
+            </div>
+        </section>
     );
 }
